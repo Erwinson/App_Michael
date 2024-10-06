53c08775cc25401e028075576dcf1b57dfcd45d5
erwinaja <erwinsonaja@gmail.com>
2024-10-06 15:36:19 +0700

    first commit

---
+++ api.dart
@@ -0,0 +1,7 @@
+class Baseurl {
+  static String tambah = "http://192.168.1.3/biodata/controller/insert.php";
+  static String data = "http://192.168.1.3/biodata/controller/list.php";
+  static String lihat = "http://192.168.1.3/biodata/controller/details.php";
+  static String edit = "http://192.168.1.3/biodata/controller/update.php";
+  static String hapus = "http://192.168.1.3/biodata/controller/delete.php";
+}
